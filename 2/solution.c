baf074459182396cf8e1d109d0ce2650ce207f05
davinchi <igselyak@gmail.com>
2024-04-22 01:14:50 +0300

    решил проблему с exit

--- 2/solution.c
+++ 2/solution.c
@@ -22,7 +22,7 @@ void cmd_exit(const struct expr *e) {
     exit(atoi(e->cmd.args[0]));
 }
 
-void execute_base_command(const struct expr *e) {
+int execute_base_command(const struct expr *e) {
     char *exec_args[e->cmd.arg_count + 2];
     exec_args[0] = e->cmd.exe;
 
@@ -30,13 +30,10 @@ void execute_base_command(const struct expr *e) {
         exec_args[i + 1] = e->cmd.args[i];
 
     exec_args[e->cmd.arg_count + 1] = NULL;
-    execvp(e->cmd.exe, exec_args); // просто с cmd.args не работает
-    perror("execvp");
+    return execvp(e->cmd.exe, exec_args);
 }
 
-
-
-static void execute_command(const struct command_line *line) {
+static int execute_command(const struct command_line *line) {
     const struct expr *e = line->head;
     if (e->type == EXPR_TYPE_COMMAND) {
         pid_t pid;
@@ -46,24 +43,21 @@ static void execute_command(const struct command_line *line) {
             if (e->next && e->next->type == EXPR_TYPE_PIPE) {
                 if (pipe(pipe_fd) == -1) {
                     perror("pipe");
-                    exit(EXIT_FAILURE);
+                    return EXIT_FAILURE;
                 }
 
                 pid = fork();
 
                 if (pid == -1) {
                     perror("fork");
-                    exit(EXIT_FAILURE);
+                    return EXIT_FAILURE;
                 } else if (pid == 0) {
                     close(pipe_fd[0]);
 
                     dup2(pipe_fd[1], STDOUT_FILENO);
                     close(pipe_fd[1]);
 
-                    execute_base_command(e);
-
-                    perror("execvp");
-                    exit(EXIT_FAILURE);
+                    return execute_base_command(e);
                 } else {
                     close(pipe_fd[1]);
 
@@ -87,13 +81,13 @@ static void execute_command(const struct command_line *line) {
 
                 if (pid == -1) {
                     perror("fork");
-                    exit(EXIT_FAILURE);
+                    return EXIT_FAILURE;
                 } else if (pid == 0) {
                     if (line->out_type == OUTPUT_TYPE_FILE_NEW || line->out_type == OUTPUT_TYPE_FILE_APPEND) {
                         dup2(output_fd, STDOUT_FILENO);
                         close(output_fd);
                     }
-                    execute_base_command(e);
+                    return execute_base_command(e);
                 } else {
                     if (!line->is_background) {
                         waitpid(pid, NULL, 0);
@@ -104,81 +98,52 @@ static void execute_command(const struct command_line *line) {
             }
         }
     }
+    return 0;
 }
 
+static int execute_command_line(const struct command_line *line) {
 
-
-
-
-
-
-static void
-execute_command_line(const struct command_line *line)
-{
-	assert(line != NULL);
-    const struct expr *e = line->head;
     if (line->head->cmd.exe != NULL && !strcmp(line->head->cmd.exe, "cd")) {
         cmd_cd(line->head);
     } else if (line->head->cmd.exe != NULL && !strcmp(line->head->cmd.exe, "exit")) {
         cmd_exit(line->head);
     } else {
-            execute_command(line);
+//        pid_t pid = fork();
+//        if (pid == 0) {
+//            return execute_command(line);
+//        } else {
+//            wait(NULL);
+//        }
+            return execute_command(line);
     }
-    printf("================================\n");
-	printf("Command line:\n");
-	printf("Is background: %d\n", (int)line->is_background);
-	printf("Output: ");
-	if (line->out_type == OUTPUT_TYPE_STDOUT) {
-		printf("stdout\n");
-	} else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-		printf("new file - \"%s\"\n", line->out_file);
-	} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-		printf("append file - \"%s\"\n", line->out_file);
-	} else {
-		assert(false);
-	}
-	printf("Expressions:\n");
-    while (e != NULL) {
-		if (e->type == EXPR_TYPE_COMMAND) {
-			printf("\tCommand: %s", e->cmd.exe);
-			for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-				printf(" %s", e->cmd.args[i]);
-			printf("\n");
-		} else if (e->type == EXPR_TYPE_PIPE) {
-			printf("\tPIPE\n");
-		} else if (e->type == EXPR_TYPE_AND) {
-			printf("\tAND\n");
-		} else if (e->type == EXPR_TYPE_OR) {
-			printf("\tOR\n");
-		} else {
-			assert(false);
-		}
-		e = e->next;
-	}
+    return 0;
 }
 
-int
-main(void)
-{
-	const size_t buf_size = 1024;
-	char buf[buf_size];
-	int rc;
-	struct parser *p = parser_new();
-	while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
-		parser_feed(p, buf, rc);
-		struct command_line *line = NULL;
-		while (true) {
-			enum parser_error err = parser_pop_next(p, &line);
-			if (err == PARSER_ERR_NONE && line == NULL)
-				break;
-			if (err != PARSER_ERR_NONE) {
-				printf("Error: %d\n", (int)err);
-				continue;
-			}
-			execute_command_line(line);
-			command_line_delete(line);
-		}
-	}
-	parser_delete(p);
-	return 0;
+int main(void) {
+    int exit_code = 0;
+    const size_t buf_size = 1024;
+    char buf[buf_size];
+    int rc;
+    struct parser *p = parser_new();
+    while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
+        parser_feed(p, buf, rc);
+        struct command_line *line = NULL;
+        while (exit_code == 0) {
+            enum parser_error err = parser_pop_next(p, &line);
+            if (err == PARSER_ERR_NONE && line == NULL)
+                break;
+            if (err != PARSER_ERR_NONE) {
+                fprintf(stderr, "Error: %d\n", (int)err);
+                continue;
+            }
+            exit_code = execute_command_line(line);
+            command_line_delete(line);
+        }
+    }
+    if (rc < 0) {
+        perror("read");
+        exit_code = EXIT_FAILURE;
+    }
+    parser_delete(p);
+    return exit_code;
 }
